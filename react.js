a405caed4810589d637bf3ca168c414fe0eb88f7
Vlad1023 <volkovskiy4@gmail.com>
2020-02-19 22:34:44 +0200

    updated

--- react.js
+++ react.js
@@ -1,26 +1,23 @@
-export function bind(domEl,model,propName){
-  let domElement = document.body.querySelector(domEl);
-  let func = function() {onEvent(event,model,propName);}
+export function bind(domID,model,propName){
+  let domElement = document.body.querySelector(domID);
+  let onEventHolder = function() {onEvent(event,model,propName);}
+
+  if(model[propName]) // if property already exist
+    domElement.value = model[propName];
+  
+
     Object.defineProperty(model, propName, {
         get(){
         return model['_' + propName];
         },
         set(value) {
         model['_' + propName] = value;  
-        domElement.removeEventListener('change',func);
+        domElement.removeEventListener('change',onEventHolder);
         domElement.value = value;
-        domElement.addEventListener('change',func);
+        domElement.addEventListener('change',onEventHolder);
         }
       });
-      if(model[propName]){
-      domElement.value = model[propName];
-      console.log("propery exist");
-      }
-      
-
-      domElement.addEventListener("click", func , false);  
-
-     
+      domElement.addEventListener("click", onEventHolder);      
 }
 function onEvent(event,model,propName){
   if(event.target.checkValidity())
